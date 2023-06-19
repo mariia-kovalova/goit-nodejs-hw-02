fa4374f6648bd0c82dc7cbe4403cdd5170e7c280
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-06-19 23:16:10 +0300

    hw03

--- controllers/contatcs.js
+++ controllers/contatcs.js
@@ -1,40 +1,53 @@
-const contacts = require('../models/contacts');
+const { Contact } = require('../models/contact');
 const { HttpError, ctrlWrapper } = require('../utils');
 
 const getAll = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find({}, '-createdAt -updatedAt');
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) throw HttpError(404, 'Not found');
   res.json(result);
 };
 
 const add = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
-};
-
-const remove = async (req, res) => {
-  const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
-  if (!result) throw HttpError(404, 'Not found');
-  res.json(result);
 };
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
-  res.status(201).json(result);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) throw HttpError(404, 'Not found');
+  res.json(result);
+};
+
+const updateFavoriteById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) throw HttpError(404, 'Not found');
+  res.json(result);
+};
+
+const remove = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) throw HttpError(404, 'Not found');
+  res.json(result);
 };
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
-  remove: ctrlWrapper(remove),
   updateById: ctrlWrapper(updateById),
+  updateFavoriteById: ctrlWrapper(updateFavoriteById),
+  remove: ctrlWrapper(remove),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const ctrl = require('../../controllers/contatcs');
-const { validateBody } = require('../../middlewares');
-const schemas = require('../../schemas/contacts');
+const { validateBody, isValidId } = require('../../middlewares');
+const { schemas } = require('../../models/contact');
 
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
 
-router.get('/:contactId', ctrl.getById);
+router.get('/:contactId', isValidId, ctrl.getById);
 
 router.post('/', validateBody(schemas.addSchema), ctrl.add);
 
-router.delete('/:contactId', ctrl.remove);
+router.put(
+  '/:contactId',
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateById);
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavoriteById
+);
+
+router.delete('/:contactId', isValidId, ctrl.remove);
 
 module.exports = router;
